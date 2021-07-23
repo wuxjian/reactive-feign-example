13d6f62ae743ad0742df3d5772d6698b7ca21d3e
wuxjian <xjwu@csih.cn>
2021-07-23 14:48:17 +0800

    使用nacos作为注册中心

--- consumer/src/main/java/the/wuxjian/consumer/ConsumerApplication.java
+++ consumer/src/main/java/the/wuxjian/consumer/ConsumerApplication.java
@@ -2,16 +2,13 @@ package the.wuxjian.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RestController;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 /**
  * Created by wuxjian 2021/7/21
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableReactiveFeignClients
 @EnableFeignClients
 public class ConsumerApplication {
