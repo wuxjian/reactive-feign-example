9d6566ed3d66ba0a5bbbf7a0375a5896120d03c2
wuxjian <xjwu@csih.cn>
2021-07-23 14:29:00 +0800

    spring boot和spring cloud版本与公司保持一致

--- consumer/src/main/java/the/wuxjian/consumer/ConsumerApplication.java
+++ consumer/src/main/java/the/wuxjian/consumer/ConsumerApplication.java
@@ -2,15 +2,18 @@ package the.wuxjian.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RestController;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 /**
  * Created by wuxjian 2021/7/21
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableReactiveFeignClients
-@EnableDiscoveryClient
+@EnableFeignClients
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
